2c6cb5134a6007cc3565cfc816015d95dce716e2
Vitória <jeanevitoria994@gmail.com>
2024-10-28 16:03:32 -0300

    feat: conexão dos controladores de auth com os services.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,25 @@
 import { loginService, cadastroService } from '../services/authService.js'
 
 export const login = async (req, res) => {
-    try {
+    const data = req.body;
 
-        return res.status(200).json(data)
-    } catch (error){
-        return res.status(500).json(error.message)
-    }
+    loginService(data)
+        .then((result) => {
+            return res.status(200).json(data)
+        })
+        .catch((error) => {
+            return res.status(500).json(error.message)
+        })
 }
 
 export const cadastro = async (req, res) => {
-    try {
+    const data = req.body;
 
-        return res.status(200).json(data)
-    } catch (error){
-        return res.status(500).json(error.message)
-    }
+    cadastroService(data)
+        .then((result) => {
+            return res.status(200).json(data)
+        })
+        .catch((error) => {
+            return res.status(500).json(error.message)
+        })
 }
