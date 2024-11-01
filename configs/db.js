7d8863bf7c07778e927727a1d8b671d1a509749a
Vitória <jeanevitoria994@gmail.com>
2024-10-31 22:43:31 -0300

    fix: conexão com o banco de dados.

--- configs/db.js
+++ configs/db.js
@@ -2,16 +2,16 @@ import { MongoClient } from 'mongodb';
 
 let dbConnection;
 
-export const connectToDB = (cb) => {
-    MongoClient.connect('mongodb+srv://adotepetDB:oVj9vFCFHfzZ415m@cluster0.nzvrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    .then((client) => {
-        dbConnection = client.db();
-        return cb();
-    })
-    .catch(err => {
-        console.log(err);
-        return cb(err);
-    });
+export const connectToDB = async () => {
+    return MongoClient.connect('mongodb+srv://adotepetDB:oVj9vFCFHfzZ415m@cluster0.nzvrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then((client) => {
+            dbConnection = client.db();
+            console.log("Conectado ao MongoDB");
+        })
+        .catch(err => {
+            console.error("Erro ao conectar ao MongoDB:", err);
+            throw err;
+        });
 };
 
 export const getDb = () => dbConnection;
