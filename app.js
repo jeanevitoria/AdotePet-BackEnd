872ddb013e218f7eeef0dd02ad8d717a9c55c6b7
Vitória <jeanevitoria994@gmail.com>
2024-10-31 22:35:03 -0300

    fix: configuração da conexão com o banco de dados.

--- app.js
+++ app.js
@@ -6,36 +6,35 @@ import cors from 'cors';
 import express from 'express';
 import { connectToDB } from './configs/db.js';
 
-// init app & middleware
-const app = express()
+// Inicializa a aplicação
+const app = express();
 
 // Middleware
 app.use(express.json());
 
-// conexão com o banco de dados mongodb
-connectToDB((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
-    return; // Não inicializa o servidor se houver um erro
-  }
-})
+// Conexão com o banco de dados mongodb
+connectToDB()
+    .then(() => {
+        // CORS para o frontend
+        app.use(cors({
+            origin: ['https://adotepet-six.vercel.app', 'http://localhost:5174'],
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        }));
 
-// CORS para o frontend
-app.use(cors({
-  origin: ['https://adotepet-six.vercel.app', 'http://localhost:5174'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-}));
+        app.get('/', (req, res) => {
+            res.send('API funcionando corretamente');
+        });
 
-app.get('/', (req, res) => {
-  res.send('API funcionando corretamente');
-});
-
-
-// routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/animal', animalRoutes);
+        // Rotas
+        app.use('/api/auth', authRoutes);
+        app.use('/api/user', userRoutes);
+        app.use('/api/chat', chatRoutes);
+        app.use('/api/animal', animalRoutes);
+    })
+    .catch(err => {
+        console.error("Erro ao conectar ao banco de dados:", err);
+        process.exit(1); // Sai do processo se a conexão falhar
+    });
 
 export default app;
