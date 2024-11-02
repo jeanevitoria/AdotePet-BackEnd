4b8f86e332155c93ddfa52353605b9a5bf87b1f7
Vitória <jeanevitoria994@gmail.com>
2024-11-02 00:17:04 -0300

    fix: retornando com o código anterior.

--- app.js
+++ app.js
@@ -7,38 +7,32 @@ import express from 'express';
 import { connectToDB } from './configs/db.js';
 
 // init app & middleware
-const app = express();
+const app = express()
 
 // Middleware
 app.use(express.json());
 
-// Conexão com o banco de dados MongoDB
+// conexão com o banco de dados mongodb
 connectToDB((err) => {
   if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err); // Log de erro
+    console.error("Erro ao conectar ao banco de dados:", err);
     return; // Não inicializa o servidor se houver um erro
   }
-  console.log("Conexão com o banco de dados MongoDB estabelecida com sucesso."); // Log de sucesso
-});
+})
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://adotepet-six.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Max-Age', '86400');
-  next();
-});
-
-// OPTIONS request
-app.options('/*', (req, res) => {
-  res.sendStatus(200);
-});
+// CORS para o frontend
+app.use(cors({
+  origin: 'https://adotepet-six.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+}));
 
 app.get('/', (req, res) => {
   res.send('API funcionando corretamente');
 });
 
-// Routes
+
+// routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
