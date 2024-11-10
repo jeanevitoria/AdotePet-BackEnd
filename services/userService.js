36e80acc37b7b503e7fbe56c9ed9e67476534d56
Vitória <jeanevitoria994@gmail.com>
2024-11-10 17:43:12 -0300

    fix: hash da senha em alterações do perfil.

--- services/userService.js
+++ services/userService.js
@@ -1,10 +1,35 @@
 import { ObjectId } from 'mongodb';
 import { getDb } from '../configs/db.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const SALT_ROUNDS = 10;
 
 const db = await getDb();
 
 export const atualizarPerfilService = (data, user_id) => {
-    return db.collection('user').updateOne({ _id: new ObjectId(user_id) }, { $set: data })
+    return db.collection('user').findOne({ _id: new ObjectId(user_id) })
+        .then((user) => {            
+            if (!user) {
+                throw new Error("Perfil não encontrado.");
+            }
+            const password = data.senha ? data.senha : null;
+            
+            if (password != null) {
+                return bcrypt.hash(password, SALT_ROUNDS)
+                    .then((hashedPassword) => {
+                        data.senha = hashedPassword
+                        return data
+                    })
+
+            }
+            return data;
+        })
+        .then((data) => { return db.collection('user').updateOne({ _id: new ObjectId(user_id) }, { $set: data }) })
         .then((result) => {
             if (result.modifiedCount > 0) {
                 return { success: true, result };
@@ -41,7 +66,8 @@ export const deletarPublicacaoService = (idPublicacao) => {
 
 export const getUserService = (user_id) => {
     return db.collection('user').find({ _id: new ObjectId(user_id) }).toArray()
-        .then((result) => { 
-            return result })
+        .then((result) => {
+            return result
+        })
         .catch((err) => { throw new Error(err.message) })
 }
