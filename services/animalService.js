9d2bf04772ee47663b31263d14ad786ac354818d
Vitória <jeanevitoria994@gmail.com>
2024-12-22 13:23:21 -0300

    fix: correçao da busca por id.

--- services/animalService.js
+++ services/animalService.js
@@ -47,7 +47,7 @@ export const confirmarAdocaoService = (user_id, idAnimal) => {
 
 
 export const getAnimalService = (id) => {
-    return db.collection('animal').findOne({ _id: id })
+    return db.collection('animal').findOne({ _id: new ObjectId(id) })
         .then((result) => {
             if (result) {
                 return result;
