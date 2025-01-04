2b1760ffcd446aa09e1031eda654d92a89c67d7e
Vitória <jeanevitoria994@gmail.com>
2025-01-03 22:12:34 -0300

    fix: ordem das rotas.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/animais-publicados', authenticateToken, getAnimaisPublicados);
 router.put('/alterar-perfil', authenticateToken, atualizarPerfil)
 router.delete('/deletar-publicacao', authenticateToken, deletarPublicacao)
-router.get('/perfil', authenticateToken, getUser);
 router.get('/perfil/:id', authenticateToken, getResponsavel);
+router.get('/perfil', authenticateToken, getUser);
 router.post('/comparar', authenticateToken, compararUsers);
 
 export default router;
