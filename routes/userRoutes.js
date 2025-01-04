357e1a64a7a345529daf24216f7a2a97aae5f194
Vitória <jeanevitoria994@gmail.com>
2025-01-03 22:15:06 -0300

    fix: mudança no nome da rota para evitar intersecção.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/animais-publicados', authenticateToken, getAnimaisPublicados);
 router.put('/alterar-perfil', authenticateToken, atualizarPerfil)
 router.delete('/deletar-publicacao', authenticateToken, deletarPublicacao)
-router.get('/perfil/:id', authenticateToken, getResponsavel);
+router.get('/responsavel/:id', authenticateToken, getResponsavel);
 router.get('/perfil', authenticateToken, getUser);
 router.post('/comparar', authenticateToken, compararUsers);
 
