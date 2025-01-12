f6f09d121a375f3e5a2687f060d439d69dd4081f
Vitória <jeanevitoria994@gmail.com>
2025-01-12 00:33:17 -0300

    fix: correção na comparação entre id's de usuários.

--- services/chatService.js
+++ services/chatService.js
@@ -23,8 +23,8 @@ export const saveMessageService = async (user_id, data) => {
         // Encontrar chat mutuo entre os dois usuários
         const chatMutual = await db.collection('chats').find({
             $or: [
-                { "user_1._id": user._id, "user_2._id": receptor._id },
-                { "user_1._id": receptor._id, "user_2._id": user._id }
+                { user_1: user._id, user_2: receptor._id },
+                { user_1: receptor._id, user_2: user._id }
             ]
         }).toArray();
 
